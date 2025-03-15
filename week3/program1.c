825cf40b46f355e42bfd4de223274cf690716696
Alisba04 <alisba.almora@gmail.com>
2025-03-15 13:04:51 +0530

    Create program1.c

---
+++ week3/program1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+void insertionSort(int arr[], int n, int *comparisons, int *shifts) {
+    *comparisons = 0;
+    *shifts = 0;
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        (*shifts)++; 
+
+      
+        while (j >= 0 && arr[j] > key) {
+            (*comparisons)++;
+            arr[j + 1] = arr[j];
+            j--;
+            (*shifts)++;
+        }
+
+        if (j >= 0) {
+            (*comparisons)++; 
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int T;
+    printf("Enter number of test cases: ");
+    scanf("%d", &T);
+
+    while (T--) {
+        int n;
+        printf("Enter size of array: ");
+        scanf("%d", &n);
+
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+
+        int comparisons = 0, shifts = 0;
+        insertionSort(arr, n, &comparisons, &shifts);
+
+        printf("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+
+        printf("Total comparisons: %d\n", comparisons);
+        printf("Total shifts: %d\n", shifts);
+    }
+
+    return 0;
+}
