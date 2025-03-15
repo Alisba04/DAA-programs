1d9f88cf15af9b8cb8af7d599a5c2deacf33eaba
Alisba04 <alisba.almora@gmail.com>
2025-03-15 13:34:58 +0530

    Update program3.c

--- week2/program3.c
+++ week2/program3.c
@@ -1,3 +1,6 @@
+/*Given an array of nonnegative integers, design an algorithm and a program to count the number of pairs of integers such that their difference is equal to a given key, K.*/
+
+
 #include <stdio.h>
 
 int count_pairs(int arr[], int n, int K) {
