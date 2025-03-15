bb7c6b09d6d06b275eb3d3b40e9d25d429bcdd9b
Alisba04 <alisba.almora@gmail.com>
2025-03-15 13:14:25 +0530

    Update program2.c

--- week3/program2.c
+++ week3/program2.c
@@ -1,3 +1,6 @@
+/*Given an unsorted array of integers, design an algorithm and implement a program to sort this array using selection sort. Your program should also find number of comparisons and number of swaps required.*/
+
+
 #include <stdio.h>
 
 
